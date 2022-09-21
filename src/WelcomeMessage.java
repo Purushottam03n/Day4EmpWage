022ca38a5c3957be600aaa971016b335bcfe8934
Purushottam <purushottam03n@gmail.com>
2022-09-21 16:18:19 +0530

    Welcome Message

---
+++ src/WelcomeMessage.java
@@ -0,0 +1,7 @@
+public class WelcomeMessage {
+    public static void main(String[] args){
+        System.out.println("Welcome to Day4 Employee Wage Computation");
+        Attendance a = new Attendance();
+        a.Attendance1();
+    }
+}
